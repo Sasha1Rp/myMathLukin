76dd200cf327c42d4e218f64c2654407a40529d1
aleksandrlukin28417@gmail.com <admin@510EC7.att.edu.ru>
2023-09-04 14:30:26 +0300

    Final first example

---
+++ src/main/java/org/example/myMathLukin.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class myMathLukin {
+    public static final double PI = 3.1415926535;
+    public static final double E = 2.7182818284;
+
+    public static void minus(double one) {
+       double two = one * -1;
+        System.out.println(two);
+
+
+    }
+}
