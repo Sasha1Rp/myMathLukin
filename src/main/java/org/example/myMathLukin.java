2ae5b3c28646b4420f03bc933ed7b4cb9a850cb8
aleksandrlukin28417@gmail.com <admin@510EC7.att.edu.ru>
2023-09-04 16:07:15 +0300

    Final first example

--- src/main/java/org/example/myMathLukin.java
+++ src/main/java/org/example/myMathLukin.java
@@ -3,11 +3,42 @@ package org.example;
 public class myMathLukin {
     public static final double PI = 3.1415926535;
     public static final double E = 2.7182818284;
-
-    public static void minus(double one) {
-       double two = one * -1;
-        System.out.println(two);
-
+    //1
+    public int minus(int number) {
+        return - number;
+    }
+    public  double minus(double number){
+        return - number;
+    }
+    public float minus(float number){
+        return  - number;
+    }
+    public  long minus(long number){
+        return - number;
+    }
+    //2
+    public double cotang(int angle){
+        return  0;
+    }
+    public  double arkcotang(int angle){
+        return 0;
+    }
+    public double finalRoundLength(int radius){
+        return 0;
+    }
+    public  int showSimpLeNumber(int start, int end){
+        return  0;
+    }
+    //3
+    public double sqrt(double number){
+        double tg;
+        double squareRoot = number / 2;
+        do {
+            tg = squareRoot;
+            squareRoot = (tg+(number/tg))/2;
+        }
+    while ( (tg-squareRoot)!=0);
+    return  squareRoot;
+    }
 
     }
-}
